ae17ddba0bc979870d454a59d75540e3d4cf85d0
Matthias Kader <_>
2024-01-04 03:01:26 +0100

    helper: feat: Get a suggestion for a new version number and run the git command to add this tag.

--- tests/auto_version_git.py
+++ tests/auto_version_git.py
@@ -167,6 +167,10 @@ def test_chatgpt():
 
 
 
+import re
+from typing import Union
+
+
 def test_chatgpt2_ansatz_OK():
 
     import subprocess
@@ -274,6 +278,8 @@ class Commit:
             tag (str): Tag(s) des Commits
         """
         Commit.instances.append(self)
+        self.index = Commit.instances.index(self)
+
         
         self.commit = commit
         self.message = message
@@ -291,9 +297,22 @@ class Commit:
         if all([ self.major, self.minor, self.patch]):  
             return "{}.{}.{}".format(self.major, self.minor, self.patch)
         else:
-            return "UNKNOWN"
+            return None
 
     
+    @classmethod
+    def get_last_commit(cls):
+        """
+        Gibt das neuste Commit als Objekt zurück.
+        Dies entspricht dem Nullten Element der Liste cls.instances
+
+        Returns:
+            Commit: Objekt
+        """
+        return cls.instances[0]
+    
+
+
 
     # @version.setter
     # def version(self, pNeueVersion):
@@ -320,7 +339,34 @@ class Commit:
 
         return obj
 
-               
+    @staticmethod                
+    def add_tag(tag_text:str):
+        """
+        Führt einen git-befehl aus, um für DAS NEUSTE Commit einen Tag hinzuzufügen - also eig. völlig unabhängig von self!
+
+        ### AUSBLICK: Könnte so gemacht werden, dass wirklcih für einen speziellen Commit ein tag angehangen wird...
+
+        Args:
+            tag (str): Text für den Tag
+        """
+
+
+        if input("ECHT WEITER MACHEN????! [y]") != "y":
+            db("kein tag hinzugefügt!!")
+            return
+
+        
+        # Git-Befehl zum Erstellen eines Tags
+        git_command = f"git tag '{tag_text}'"
+
+        # Ausführen des Git-Befehls im Terminal
+        try:
+            subprocess.run(git_command, shell=True, check=True)
+            print(f"Git Tag '{tag_text}' wurde erfolgreich erstellt.")
+
+        except subprocess.CalledProcessError as e:
+            print(f"Fehler beim Erstellen des Git Tags: {e}")
+        
 
 
 
@@ -384,7 +430,242 @@ class Commit:
 
 
 
+    @classmethod
+    def is_valid_version_pattern(cls, version:str) -> bool:
+        """
+        Prüft mittels regex, ob der text der semantischen Versionierung entspreicht.
+
+        Args:
+            version (str): zu überprüfender Text
+
+        Returns:
+            bool: True falls OK, False falls kein match
+        """
+
+
+        semanticVar = re.compile(f"^\d{1,}\.\d{1,}\.\d{1,}$")   
+
+
+        if semanticVar.match(version):
+            return True
+        else:
+            return False
+        
+
+
+
+    @staticmethod
+    def extract_params_from_version(version:str) -> tuple:
+        """
+        Liesst die Versionstext aus, prueft ihn auf Gueltigkeit nach dem Muster \d*\.\d*\.\d* und extrahiert bei Gültigkeit die numerischen Werte für major, minor und patch und gibt sie als tuple in dieser Reihenfolge zurück.
+        Sofern das Muster nicht korrekt ist, werden alle Parameter mit None belegt
+        """   
+
+        # # siehe extra func is_valid_version_pattern
+        # semanticVar = re.compile(f"\d{1,}\.\d{1,}\.\d{1,}")     
+        
+        # if semanticVar.match(version):
+        if Commit.is_valid_version_pattern():
+            major, minor, patch = map(int, version.split("."))
+
+        else:
+            print("!!! ERROR !!! Der uebergebene Versionstext {} entspricht nicht der KOnvention! Daher werden major, minor, patch alle mit None belegt!".format(version))
+            major, minor, patch = (None, None, None)
+
+        return (major, minor, patch)
+
+
+
+    def suggest_version(self) -> str:
+        """
+        Sucht ausgehend von einem konkreten Commit-Objekt so lange in der Historie der Commits, bis ein Commit mit tag gefunden wurde. 
+        Aktuell wird vorausgesetzt, dass die Versionsnummer die einzigen Tags sind!
+        """
+
+        index = self.index
+        # ref_tag = None
+        ref_obj:Commit = None
+
+
+
+
+
+        # =============================================================================
+        #### Suche Referenz-Commit, die bereits einen tag hat:  ####
+        # =============================================================================
+        
+
+        # REFACTOR-TODO: Auslagern als Func!
+        # Suche ab diesem Commit nach dem nächstfrüheren Commit, welches mit einem tag versehen ist:
+        while (index < len(self.instances)):
+                
+            temp_obj:Commit = self.instances[index]
+
+            if temp_obj.tag:
+                # # found commit with tag!
+                # ref_tag = temp_obj.tag
+                # ref_index = temp_obj.index
+                ref_obj = temp_obj
+                break
+
+            index += 1
+
+
+
+
+
+
+
+
+        if not ref_obj:
+            # no tag founded. --> suggest 0.0.0
+            suggestion = "0.0.0"
+
+            # break whole thing because there is nothing to analyse when there isn't an initial version number tag!
+            return suggestion
+        
+
+
+        # BEISPIEL: ref_obj.tag = "1.2.3"
+
+        # =============================================================================
+        #### Durchsuche alle vergangenen Commit-MEssages nach Stichwörtern, für die eine Versionierung durchgeführt werden müssten ####
+        # =============================================================================
+        
+            
+        # ON this point there is definitely a previous version number.
+        # So check all commit messages from the commit which follows of the founded ref_commit_object, wheather the keywords in their message is one for major, minor, patch or no version increments:
+
+
+
+        # Flags for new changes:
+        major_change = False
+        minor_change = False
+        patch_change = False
+
+
+
+
+        
+        # for index in range(self.index, ref_index - 1):
+        for index, commit_obj in enumerate(self.instances):
+            
+            commit_obj:Commit # nur für typehints
+         
+
+
+            if commit_obj == ref_obj:
+                # Angekommen an dem versionierten, tagbehafteten Commit.
+                
+                break
+
+            type_of_commit = commit_obj.message.split(":")[0].rstrip(" ").lstrip(" ").lower()
+            
+            if type_of_commit.endswith("!"):
+                # includes "feat!" AND "fix!"
+                # BREAKING CHANGE --> SHOULD BE a NEW MAJOR VERSION!
+                major_change = True
+                # There is no need to search for more changes as major changes are the most top-leveled changes which can be represented in the semantic version.
+                break
+
+            
+            elif type_of_commit == "feat":
+                minor_change = True
+
+            elif type_of_commit == "fix":
+                patch_change = True
+
+
+
+
+
+        # BEISPIEL: ref_obj.tag = "1.2.3"
+        previous_version = ref_obj.tag
+
+        major, minor, patch = self.extract_params_from_version(previous_version)
+        
+        # major, minor, patch = map(int, previous_version.split("."))
+        # FIX: ACHTUNG: Fehlervermeidung fehlt! Gültigkeit der versionsnummern werden nicht geprüft!
+
+
+
+
+
+
+        # =============================================================================
+        #### # Evaluation of the previous changes since the last tagged commit: ####
+        # =============================================================================
+        
+        # noch nicht herausspringen jeweils, da noch ein weiteres Attibut gesetzt werden muss!
+        if major_change:
+            suggestion = "{}.0.0".format(major + 1)
+            # return suggestion
+        
+        elif minor_change:
+            suggestion = "0.{}.0".format(minor + 1)
+            # return suggestion
+        
+        elif patch_change:
+            suggestion = "0.0.{}".format(patch + 1)
+            # return suggestion
+        
+        # SONST: Keine bedeutenden Modifikationen. Die letzte  Versionsnummer bleibt bestehen.
+        suggestion = previous_version
+
+        # als flag:
+        self.is_new_version = True
+
+        return suggestion
+        
+
+
+
+        
+                
+
+
+        db("debug.....")
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import subprocess
+
+
+
 
 
 
@@ -472,14 +753,61 @@ def main():
 
 
 
-    # Flags for new changes:
-    major_change = False
-    minor_change = False
-    patch_change = False
 
 
-
+    # Bisherige Commits einlesen:
     Commit.initialize()
+
+
+    # Neue Versionsnumer vorschlagen:
+    commit:Commit = Commit.get_last_commit()
+    suggested_version = commit.suggest_version()
+
+
+    db(suggested_version)
+
+
+    if not commit.is_new_version:
+
+        db(f"Keine weiteren Schritte erforderlich. Die bisherige Versionsnummer {suggested_version} bleibt bestehen.")
+
+        return False
+
+
+
+
+
+
+
+    # ELSE: tag der vorgeschlagenen Version fuer den aktuellen commit hinzufuegen
+
+
+    version = suggested_version
+    while  not Commit.is_valid_version_pattern(version):
+    
+    
+    
+    # if Commit.is_valid_version_pattern(suggested_version):
+    # #     # dann gültige Version!
+
+
+    # # commit.major, commit.minor, commit.tag = commit.extract_params_from_version(suggested_version)
+    # # if commit.version:
+
+    # else:
+        version = input("!!! ACHTUNG: Die vorgeschlagene Version ist nicht gueltig. Bitte eine gueltige Versionsnummer eingeben oder 'x' fuer Abbruch")
+        
+        if version.lower() == "x":
+            db("Programmabbruch durch Benutzter")
+
+            return False
+
+
+    # tag setzten:
+    commit.add_tag(version)
+
+
+    db(Commit)
 
     # initialize_commits()
 
